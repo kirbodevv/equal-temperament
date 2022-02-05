e9db591833d0391534fed968792cf8ccb5276991
Kirill <kirbo06@mail.ru>
2022-02-05 14:08:41 +0100

    - rewrite table building with java-markdown-generator (close #1) - readme update

--- src/main/java/com/kgc/et/Main.java
+++ src/main/java/com/kgc/et/Main.java
@@ -1,35 +1,27 @@
 package com.kgc.et;
 
-import java.text.DecimalFormat;
+import net.steppschuh.markdowngenerator.table.Table;
 
-import static com.kgc.et.EqualTemperament.B8;
-import static com.kgc.et.EqualTemperament.C0;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
         DecimalFormat format = new DecimalFormat("##.00");
 
-        StringBuilder table = new StringBuilder();
-        table.append("| Октава   | C       | C#      | D       | D#      | E       | F       | F#      | G       | G#      | A       | A#      | B       |").append("\n");
-        table.append("|:---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|---------|").append("\n");
+        Table.Builder tableBuilder = new Table.Builder()
+                .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_RIGHT)
+                .addRow("Ноты", 0, 1, 2, 3, 4, 5, 6, 7, 8);
+        String[] notes = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
-        StringBuilder octaveBuilder = new StringBuilder("| 0        |");
-        int counter = 0;
-        int octaveCounter = 0;
-        for (int note = C0; note <= B8; note++) {
-            String noteString = format.format(EqualTemperament.calculate(note));
-            octaveBuilder.append(" ")
-                    .append(noteString)
-                    .append(" ".repeat(Math.max(0, 8 - noteString.length()))).append("|");
-            counter++;
-            if (counter >= 12) {
-                counter = 0;
-                octaveCounter++;
-                table.append(octaveBuilder).append("\n");
-                octaveBuilder.delete(0, octaveBuilder.length());
-                octaveBuilder.append("| ").append(octaveCounter).append("        |");
+        for (int n = 0; n < notes.length; n++) {
+            String[] octaves = new String[10];
+            octaves[0] = notes[n];
+            for (int octave = 0; octave <= 8; octave++) {
+                octaves[octave + 1] =
+                        format.format(EqualTemperament.calculate(EqualTemperament.C0 + n + octave * 12));
             }
+            tableBuilder.addRow((Object[]) octaves);
         }
-        System.out.println(table);
+        System.out.println(tableBuilder.build());
     }
 }
